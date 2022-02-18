e19a9f7504633dcd9e06ea35c4b9bb62748249b9
Jonatas A <jonatasaraujofx@gmail.com>
2022-02-18 18:18:59 -0300

    feat: add ReCaptcha support

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -1,17 +1,17 @@
 import { initializeApp } from 'firebase/app';
-
+import { initializeAppCheck, ReCaptchaV3Provider } from "firebase/app-check"
 import { getAuth } from 'firebase/auth';
 import { getFirestore  } from 'firebase/firestore';
 
 // const firebaseConfig = {
-//   apiKey: process.env.FIREBASE_API_KEY,
-//   authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-//   databaseURL: process.env.FIREBASE_DATABASE_URL,
-//   projectId: process.env.FIREBASE_PROJECT_ID,
-//   storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-//   messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-//   appId: process.env.FIREBASE_APP_ID,
-//   measurementId: process.env.FIREBASE_MENSUREMENT_ID
+//   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+//   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+//   databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+//   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+//   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+//   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+//   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+//   measurementId: process.env.NEXT_PUBLIC_FIREBASE_MENSUREMENT_ID
 // };
 const firebaseConfig = {
   apiKey: "AIzaSyAr_CERUQ5mMUbpHwT9BxKm0MgO7xX6iH8",
@@ -24,7 +24,15 @@ const firebaseConfig = {
   measurementId: "G-GMDD8S91PC"
 };
 
-initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
+
+const appCheck = initializeAppCheck(app, {
+  provider: new ReCaptchaV3Provider(process.env.NEXT_PUBLIC_GOOGLE_RECAPTCHA_KEY),
+
+  // Optional argument. If true, the SDK automatically refreshes App Check
+  // tokens as needed.
+  isTokenAutoRefreshEnabled: true
+});
 
 const auth = getAuth();
 const firestore = getFirestore();
